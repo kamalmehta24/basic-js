c418e9048c74113b90766758a689f5b6ccd082fd
kamalmehta24 <156507994+kamalmehta24@users.noreply.github.com>
2024-01-13 10:23:26 +0000

    Basic variables

---
+++ js_basic/variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345
+let accountEmail = "kamalkumarmehtajaipur@gmail.com"
+var accountPassword = 1234567
+accountCity = "Jaipur"
+
+let accountState;
+
+accountEmail = "test@gmail.com"
+accountPassword = 987564
+
+
+console.log(accountEmail);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
